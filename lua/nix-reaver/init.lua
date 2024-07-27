625ff7251b56e3d85eb03a957e11265eba21d261
Gabe Dunn <gabe@sent.at>
2024-07-27 16:55:52 -0600

    chore: switch to stylua

--- lua/nix-reaver/init.lua
+++ lua/nix-reaver/init.lua
@@ -1,37 +1,37 @@
 local M = {}
 
 function M.setup(opts)
-  opts = opts or {
-    fetcherNames = { "fetchFromGitHub" },
-  }
+	opts = opts or {
+		fetcherNames = { 'fetchFromGitHub' },
+	}
 
-  vim.api.nvim_create_user_command("NixReaver", function()
-    local reaver = require("nix-reaver.nix-reaver")
+	vim.api.nvim_create_user_command('NixReaver', function()
+		local reaver = require('nix-reaver.nix-reaver')
 
-    local node = reaver.is_cursor_in_fetcher(opts.fetcherNames)
-    if node then
-      local owner, repo = reaver.get_owner_and_repo(node)
+		local node = reaver.is_cursor_in_fetcher(opts.fetcherNames)
+		if node then
+			local owner, repo = reaver.get_owner_and_repo(node)
 
-      local commit_hash = reaver.get_latest_commit(owner, repo)
-      if not commit_hash then
-        return
-      end
+			local commit_hash = reaver.get_latest_commit(owner, repo)
+			if not commit_hash then
+				return
+			end
 
-      reaver.update_git_rev_or_hash(node, true, commit_hash)
+			reaver.update_git_rev_or_hash(node, true, commit_hash)
 
-      local hash = reaver.get_latest_commit_hash(owner, repo, commit_hash)
-      if not hash then
-        return
-      end
+			local hash = reaver.get_latest_commit_hash(owner, repo, commit_hash)
+			if not hash then
+				return
+			end
 
-      reaver.update_git_rev_or_hash(node, false, hash)
+			reaver.update_git_rev_or_hash(node, false, hash)
 
-      -- notify after updating
-      vim.schedule(function()
-        vim.notify(string.format("Updated %s to %s", repo, hash))
-      end)
-    end
-  end, {})
+			-- notify after updating
+			vim.schedule(function()
+				vim.notify(string.format('Updated %s to %s', repo, hash))
+			end)
+		end
+	end, {})
 end
 
 return M
--- lua/nix-reaver/nix-reaver.lua
+++ lua/nix-reaver/nix-reaver.lua
@@ -1,155 +1,155 @@
 local M = {}
-local ts_utils = require("nvim-treesitter.ts_utils")
+local ts_utils = require('nvim-treesitter.ts_utils')
 
 --- @param fetchers string[]
 --- @return TSNode|nil
 function M.is_cursor_in_fetcher(fetchers)
-  -- Ensure Treesitter is available
-  if not vim.treesitter then
-    print("Treesitter not available")
-    return nil
-  end
+	-- Ensure Treesitter is available
+	if not vim.treesitter then
+		print('Treesitter not available')
+		return nil
+	end
 
-  -- Get the current node
-  local bufnr = vim.api.nvim_get_current_buf()
-  local node = ts_utils.get_node_at_cursor()
+	-- Get the current node
+	local bufnr = vim.api.nvim_get_current_buf()
+	local node = ts_utils.get_node_at_cursor()
 
-  -- Traverse up the tree to find a function call node
-  while node do
-    if node:type() == "apply_expression" then
-      local child = node:child(0)
-      if
-        child
-        and child:type() == "variable_expression"
-        and vim.treesitter.get_node_text(child, bufnr) == "fetchFromGitHub"
-        -- TODO: Add support for other fetchers
-      then
-        local params = node:child(1)
-        if params then
-          return params
-        end
-      end
-    end
-    node = node:parent()
-  end
+	-- Traverse up the tree to find a function call node
+	while node do
+		if node:type() == 'apply_expression' then
+			local child = node:child(0)
+			if
+				child
+				and child:type() == 'variable_expression'
+				and vim.treesitter.get_node_text(child, bufnr) == 'fetchFromGitHub'
+				-- TODO: Add support for other fetchers
+			then
+				local params = node:child(1)
+				if params then
+					return params
+				end
+			end
+		end
+		node = node:parent()
+	end
 
-  return nil
+	return nil
 end
 
 --- @param node TSNode
 function M.get_owner_and_repo(node)
-  local set = node:child(1)
+	local set = node:child(1)
 
-  local owner = nil
-  local repo = nil
+	local owner = nil
+	local repo = nil
 
-  if set and set:type() == "binding_set" then
-    local bindings = set:named_children()
-    for _, binding in ipairs(bindings) do
-      local key = binding:child(0)
-      local value = binding:child(2)
+	if set and set:type() == 'binding_set' then
+		local bindings = set:named_children()
+		for _, binding in ipairs(bindings) do
+			local key = binding:child(0)
+			local value = binding:child(2)
 
-      if key and value and key:type() == "attrpath" and value:type() == "string_expression" then
-        local k = vim.treesitter.get_node_text(key, 0)
-        local v = vim.treesitter.get_node_text(value, 0)
+			if key and value and key:type() == 'attrpath' and value:type() == 'string_expression' then
+				local k = vim.treesitter.get_node_text(key, 0)
+				local v = vim.treesitter.get_node_text(value, 0)
 
-        if k == "owner" then
-          owner = v
-        elseif k == "repo" then
-          repo = v
-        end
-      end
-    end
-  end
+				if k == 'owner' then
+					owner = v
+				elseif k == 'repo' then
+					repo = v
+				end
+			end
+		end
+	end
 
-  if owner and repo then
-    return owner, repo
-  end
+	if owner and repo then
+		return owner, repo
+	end
 end
 
 --- @param owner string
 --- @param repo string
 function M.get_latest_commit(owner, repo)
-  local repo_url = string.format("https://github.com/%s/%s", owner, repo)
+	local repo_url = string.format('https://github.com/%s/%s', owner, repo)
 
-  -- run git ls-remote to get the latest commit hash
-  local handle = io.popen(string.format("git ls-remote %s HEAD 2>/dev/null", repo_url))
-  if not handle then
-    print("Failed to run git ls-remote")
-    return
-  end
+	-- run git ls-remote to get the latest commit hash
+	local handle = io.popen(string.format('git ls-remote %s HEAD 2>/dev/null', repo_url))
+	if not handle then
+		print('Failed to run git ls-remote')
+		return
+	end
 
-  local output = handle:read("*a")
-  handle:close()
+	local output = handle:read('*a')
+	handle:close()
 
-  local commit_hash = vim.split(output, "\t")[1]
+	local commit_hash = vim.split(output, '\t')[1]
 
-  if not commit_hash then
-    print("Failed to get commit hash")
-    return
-  end
+	if not commit_hash then
+		print('Failed to get commit hash')
+		return
+	end
 
-  return commit_hash
+	return commit_hash
 end
 
 --- @param owner string
 --- @param repo string
 --- @param rev string
 function M.get_latest_commit_hash(owner, repo, rev)
-  -- TODO: add support for other branches, tags, etc
-  local cmd_args = string.format("https://github.com/%s/%s %s", owner, repo, rev)
+	-- TODO: add support for other branches, tags, etc
+	local cmd_args = string.format('https://github.com/%s/%s %s', owner, repo, rev)
 
-  -- run git ls-remote to get the latest commit hash
-  local handle = io.popen(string.format("nurl -H %s 2>/dev/null", cmd_args))
-  if not handle then
-    print("Failed to run nurl")
-    return
-  end
+	-- run git ls-remote to get the latest commit hash
+	local handle = io.popen(string.format('nurl -H %s 2>/dev/null', cmd_args))
+	if not handle then
+		print('Failed to run nurl')
+		return
+	end
 
-  --- @type string
-  local output = handle:read("*a")
-  handle:close()
+	--- @type string
+	local output = handle:read('*a')
+	handle:close()
 
-  -- remove the leading and trailing whitespace
-  local hash = output:gsub("^%s*(.-)%s*$", "%1")
+	-- remove the leading and trailing whitespace
+	local hash = output:gsub('^%s*(.-)%s*$', '%1')
 
-  if not hash then
-    print("Failed to get output hash")
-    return
-  end
+	if not hash then
+		print('Failed to get output hash')
+		return
+	end
 
-  return hash
+	return hash
 end
 
 --- @param node TSNode
 --- @param commit_hash string
 function M.update_git_rev_or_hash(node, is_rev, commit_hash)
-  local set = node:child(1)
-  if set and set:type() == "binding_set" then
-    local bindings = set:named_children()
-    for _, binding in ipairs(bindings) do
-      local key = binding:child(0)
-      local value = binding:child(2)
+	local set = node:child(1)
+	if set and set:type() == 'binding_set' then
+		local bindings = set:named_children()
+		for _, binding in ipairs(bindings) do
+			local key = binding:child(0)
+			local value = binding:child(2)
 
-      if key and value and key:type() == "attrpath" and value:type() == "string_expression" then
-        local k = vim.treesitter.get_node_text(key, 0)
+			if key and value and key:type() == 'attrpath' and value:type() == 'string_expression' then
+				local k = vim.treesitter.get_node_text(key, 0)
 
-        local node_name = is_rev and "rev" or "hash"
+				local node_name = is_rev and 'rev' or 'hash'
 
-        if k == node_name then
-          -- goto the node
-          ts_utils.goto_node(value)
+				if k == node_name then
+					-- goto the node
+					ts_utils.goto_node(value)
 
-          -- get position of the string
-          local start_row, start_col, end_row, end_col = value:range()
-          -- print(start_row, start_col, end_row, end_col)
+					-- get position of the string
+					local start_row, start_col, end_row, end_col = value:range()
+					-- print(start_row, start_col, end_row, end_col)
 
-          -- replace the content between the quotes
-          vim.api.nvim_buf_set_text(0, start_row, start_col + 1, end_row, end_col - 1, { commit_hash })
-        end
-      end
-    end
-  end
+					-- replace the content between the quotes
+					vim.api.nvim_buf_set_text(0, start_row, start_col + 1, end_row, end_col - 1, { commit_hash })
+				end
+			end
+		end
+	end
 end
 
 return M
